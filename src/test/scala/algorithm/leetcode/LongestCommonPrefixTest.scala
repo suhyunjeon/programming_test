c19166543cc738971ef1c130a6aacd60913e5623
suhyunjeon <suhyunjun@gmail.com>
2018-12-23 21:07:33 +0900

    println 제거

--- src/test/scala/algorithm/leetcode/LongestCommonPrefixTest.scala
+++ src/test/scala/algorithm/leetcode/LongestCommonPrefixTest.scala
@@ -7,13 +7,10 @@ import algorithm.leetcode.LongestCommonPrefix.longestCommonPrefix
 class LongestCommonPrefixTest extends FunSuite {
 
   test("longest common prefix 1") {
-    println(longestCommonPrefix(Array("flower","flow","flight")))
     longestCommonPrefix(Array("flower","flow","flight")) should be ("fl")
   }
 
   test("longest common prefix 2") {
-    println(longestCommonPrefix(Array("dog","racecar","car")))
     longestCommonPrefix(Array("dog","racecar","car")) should be ("")
   }
-
 }
